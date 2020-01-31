9a14a0ba00e063842e66319f0e4b290529ad699a
Arvin Agas <arvagas@gmail.com>
2020-01-31 12:07:33 -0500

    refactored previous code; beautify run loop

--- cpu.py
+++ cpu.py
@@ -2,14 +2,6 @@
 
 import sys
 
-alu_dict = {
-    0b10100000: 'ADD',
-    0b10100001: 'SUB',
-    0b10100010: 'MUL',
-    0b10100011: 'DIV',
-    0b10100100: 'MOD'
-}
-
 class CPU:
     """Main CPU class."""
 
@@ -19,15 +11,104 @@ class CPU:
         self.reg = [0] * 8 # Register
         self.pc = 0 # Program counter; points to the current instruction
         self.sp = 7 # Stack pointer; lives in register spot 7
+        self.running = True # Makes sure program is running
 
-        ########## INSTRUCTION HANDLERS ##########
-        self.LDI  = 0b10000010
-        self.PRN  = 0b01000111
-        self.HLT  = 0b00000001
-        self.PUSH = 0b01000101
-        self.POP  = 0b01000110
-        self.CALL = 0b01010000
-        self.RET  = 0b00010001
+        def LDI(operand_a, operand_b):
+            # Insert a decimal integer into a register
+            self.reg[operand_a] = operand_b
+            self.pc += 3
+
+        def PRN(operand_a, operand_b):
+            # Print to the console the decimal integer value that is
+            # stored in the given register.
+            print(self.reg[operand_a])
+            self.pc += 2
+        
+        def HLT(operand_a, operand_b):
+            # Halt the CPU (and exit the emulator)
+            self.running = False
+
+        def PUSH(operand_a, operand_b):
+            """
+            Push the value in the given register on the stack.
+                1. Decrement the SP.
+                2. Copy the value in the given register to the address
+                pointed to by SP.
+            """
+            self.sp -= 1
+            self.ram[self.sp] = self.reg[operand_a]
+            self.pc += 2
+        
+        def POP(operand_a, operand_b):
+            """
+            Pop the value at the top of stack into the given register.
+                1. Copy the value from the address pointed to by SP to
+                the given register.
+                2. Increment SP.
+            """
+            self.reg[operand_a] = self.ram[self.sp]
+            self.sp += 1
+            self.pc += 2
+
+        def CALL(operand_a, operand_b):
+            """
+            Calls a subroutine at the address stored in the register.
+                1. The address of the instruction directly after CALL is
+                pushed onto the stack. This allows us to return to where
+                we left off when the subroutine finishes executing.
+                2. The PC is set to the address stored in the given
+                register. We jump to that location in RAM and execute the
+                first instruction in the subroutine. The PC can move
+                forward or backwards from its current location.
+            """
+            # Push return address onto the stack
+            return_address = self.pc + 2
+            self.reg[self.sp] -= 1
+            self.ram[self.reg[self.sp]] = return_address
+            # Set PC to the value in the register
+            self.pc = self.reg[operand_a]
+            
+        def RET(operand_a, operand_b):
+            # Return from subroutine.
+            # Pop the value from the top of the stack and store it in the PC.
+            self.pc = self.ram[self.reg[self.sp]]
+            self.reg[self.sp] += 1
+        
+        def ADD(operand_a, operand_b):
+            self.alu('ADD', operand_a, operand_b)
+            self.pc += 3
+
+        def SUB(operand_a, operand_b):
+            self.alu('SUB', operand_a, operand_b)
+            self.pc += 3
+
+        def MUL(operand_a, operand_b):
+            self.alu('MUL', operand_a, operand_b)
+            self.pc += 3
+
+        def DIV(operand_a, operand_b):
+            self.alu('DIV', operand_a, operand_b)
+            self.pc += 3
+
+        def MOD(operand_a, operand_b):
+            self.alu('MOD', operand_a, operand_b)
+            self.pc += 3
+        
+        self.op_codes = {
+            0b10000010: LDI,
+            0b01000111: PRN,
+            0b00000001: HLT,
+            0b01000101: PUSH,
+            0b01000110: POP,
+            0b01010000: CALL,
+            0b00010001: RET,
+            # ALU
+            0b10100000: ADD,
+            0b10100001: SUB,
+            0b10100010: MUL,
+            0b10100011: DIV,
+            0b10100100: MOD
+        }
 
     def load(self):
         """Load a program into memory."""
@@ -115,73 +196,19 @@ class CPU:
     def ram_write(self, MAR, MDR):
         self.ram[MAR] = MDR
 
-    # Set the value of a register to an integer.
-
     def run(self):
         """Run the CPU."""
-        while True:
+        while self.running:
             # Instruction Register (IR)
             IR = self.ram_read(self.pc)
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
+
+            # Find opcode name of IR
+            opcode = self.op_codes[IR]
             
-            if IR is self.LDI:
-                # Insert a decimal integer into a register
-                self.reg[operand_a] = operand_b
-                self.pc += 3
-            elif IR is self.PRN:
-                # Print to the console the decimal integer value that is
-                # stored in the given register.
-                print(self.reg[operand_a])
-                self.pc += 2
-            elif IR is self.PUSH:
-                """
-                Push the value in the given register on the stack.
-                    1. Decrement the SP.
-                    2. Copy the value in the given register to the address
-                    pointed to by SP.
-                """
-                self.sp -= 1
-                self.ram[self.sp] = self.reg[operand_a]
-                self.pc += 2
-            elif IR is self.POP:
-                """
-                Pop the value at the top of stack into the given register.
-                    1. Copy the value from the address pointed to by SP to
-                    the given register.
-                    2. Increment SP.
-                """
-                self.reg[operand_a] = self.ram[self.sp]
-                self.sp += 1
-                self.pc += 2
-            elif IR is self.CALL:
-                """
-                Calls a subroutine at the address stored in the register.
-                    1. The address of the instruction directly after CALL is
-                    pushed onto the stack. This allows us to return to where
-                    we left off when the subroutine finishes executing.
-                    2. The PC is set to the address stored in the given
-                    register. We jump to that location in RAM and execute the
-                    first instruction in the subroutine. The PC can move
-                    forward or backwards from its current location.
-                """
-                # Push return address onto the stack
-                return_address = self.pc + 2
-                self.reg[self.sp] -= 1
-                self.ram[self.reg[self.sp]] = return_address
-                # Set PC to the value in the register
-                self.pc = self.reg[operand_a]
-            elif IR is self.RET:
-                # Return from subroutine.
-                # Pop the value from the top of the stack and store it in the PC.
-                self.pc = self.ram[self.reg[self.sp]]
-                self.reg[self.sp] += 1
-            elif IR in alu_dict:
-                self.alu(alu_dict[IR], operand_a, operand_b)
-                self.pc += 3
-            elif IR is self.HLT:
-                # Halt the CPU (and exit the emulator)
-                break
+            if opcode:
+                opcode(operand_a, operand_b)
             else:
                 print(f'Error: Unknown command: {IR}')
                 sys.exit(1)
